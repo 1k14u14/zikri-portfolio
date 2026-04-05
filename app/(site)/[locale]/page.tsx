9134c75fcbee3db700d45a5e0dfb4b967fc4833d
zikri aulia <1k14u14@gmail.com>
2026-04-05 11:46:49 +0700

    change: grid main layout

--- app/(site)/[locale]/page.tsx
+++ app/(site)/[locale]/page.tsx
@@ -24,44 +24,45 @@ export default async function Home({ params }: { params: Promise<{ locale: strin
       1 column on mobile, 4 columns on large desktop screens.
       This replaces the broken 'flex-8' approach.
     */
-    <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 relative">
-      
-      {/* MAIN CONTENT AREA: Takes up 3 out of 4 columns on desktop */}
-      <div className="lg:col-span-3">
-        <Hero locale={currentLocale} />
-        <Projects locale={currentLocale} />
-        <About locale={currentLocale} />
-        <Service locale={currentLocale} />
-        <Contact />
-      </div>
-
-      {/* TABLE OF CONTENTS / SIDEBAR 
-        'hidden lg:block' means it disappears on mobile, but shows on laptops.
-      */}
-      <aside className="hidden lg:block lg:col-span-1 mt-20">
+    <div>
+      <Hero locale={currentLocale} />
+      <div className="grid grid-cols-1 lg:grid-cols-8 gap-8 relative">
         
-        {/* sticky top-20 makes it float smoothly down the screen as the user scrolls */}
-        <ul className="pl-6 border-l border-border sticky top-20 flex flex-col gap-4 text-sm text-slate-500 font-medium">
-          
-          <li className="hover:text-primary hover:translate-x-1 transition-all">
-            <a href="#projects">{t('projects')}</a> {/* Replace with {t('projects')} if translated! */}
-          </li>
-          
-          <li className="hover:text-primary hover:translate-x-1 transition-all">
-            <a href="#about">{t('about')}</a>
-          </li>
-          
-          <li className="hover:text-primary hover:translate-x-1 transition-all">
-            <a href="#service">{t('service')}</a>
-          </li>
-          
-          <li className="hover:text-primary hover:translate-x-1 transition-all">
-            <a href="#contact">{t('contact')}</a>
-          </li>
-          
-        </ul>
-      </aside>
+        {/* MAIN CONTENT AREA: Takes up 7 out of 8 columns on desktop */}
+        <div className="lg:col-span-7">
+          <Projects locale={currentLocale} />
+          <About locale={currentLocale} />
+          <Service locale={currentLocale} />
+          <Contact />
+        </div>
 
+        {/* TABLE OF CONTENTS / SIDEBAR 
+          'hidden lg:block' means it disappears on mobile, but shows on laptops.
+        */}
+        <aside className="hidden lg:block lg:col-span-1 mt-20">
+          
+          {/* sticky top-20 makes it float smoothly down the screen as the user scrolls */}
+          <ul className="pl-6 border-l border-border sticky top-20 flex flex-col gap-4 text-sm text-slate-500 font-medium">
+            
+            <li className="hover:text-primary hover:translate-x-1 transition-all">
+              <a href="#projects">{t('projects')}</a> {/* Replace with {t('projects')} if translated! */}
+            </li>
+            
+            <li className="hover:text-primary hover:translate-x-1 transition-all">
+              <a href="#about">{t('about')}</a>
+            </li>
+            
+            <li className="hover:text-primary hover:translate-x-1 transition-all">
+              <a href="#service">{t('service')}</a>
+            </li>
+            
+            <li className="hover:text-primary hover:translate-x-1 transition-all">
+              <a href="#contact">{t('contact')}</a>
+            </li>
+            
+          </ul>
+        </aside>
+      </div>
     </div>
   );
 }
